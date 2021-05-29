ea76d6f2c658122d64517f445190aafd1e5b1e5f
saeed_raeisi <saeed.raesi2020gmail.com>
2021-05-29 12:36:22 -0600

    ver-1.5

--- Alice_v1.0.ino
+++ Alice_v1.0.ino
@@ -5,6 +5,34 @@ Motor myMotor;
 int dis_state=0;
 int motion_state=0;
 void setup() {
+  /* header set all library and class name and define 
+   *  .c file all function stracture
+   *  
+   *  mega 
+   * body and head 
+   * body -> class wheels and hands 
+   * 
+     * wheels -> motor and some change
+     * hands -> just servo and degree
+   * 
+   * head -> eye and abro and dahan 
+   * 
+   *  eye -> eye.h
+   *  abro -> just servo 
+   *  dahan -> just lcd
+   * 
+   * uno -> get signal and paly music 
+   * 
+   * 
+   * robot after turn on    -> look right and left and strait go to less distance or go to moving object      or random move
+   * robot after see -> smile and state 1 abro
+   * robot after get wall -> sad an state 2 abro
+   * robot after not found any thing get absolate and state 3 abro
+   * 
+   * robot have random choise 
+   * 
+   * 
+  */
   Serial.begin(9600);
   setup_eye();
   setup_motion();
